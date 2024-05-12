a0ff7476311bbcfac7f653fcd414dd784e7a91c7
ChyYasir <chyyasir2000@gmail.com>
2024-05-12 23:02:05 +0600

    google analytics added

--- pages/_document.js
+++ pages/_document.js
@@ -4,21 +4,25 @@ import { Analytics } from "@vercel/analytics/react";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      {/* <!-- Google tag (gtag.js) --> */}
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-WR89C64DMP"
-      ></script>
-      <script>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-WR89C64DMP"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-          gtag('config', 'G-WR89C64DMP');
-        `}
-      </script>
+              gtag('config', 'G-WR89C64DMP');
+            `,
+          }}
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
